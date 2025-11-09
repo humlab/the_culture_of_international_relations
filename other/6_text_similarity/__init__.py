c570fada06bb6470e0263f934f2d83c74797b886
roger-mahler <roger.mahler@hotmail.com>
2025-11-09 08:23:43 +0100

    refactor: deprecated TBM NER, text similarity

--- other/6_text_similarity/__init__.py
+++
@@ -1,7 +0,0 @@
-from common.color_utility import *
-from common.utility import *
-from common.file_utility import FileUtility
-from common.wordcloud_utility import plot_wordcloud
-
-import common.widgets_utility
-import common.widgets_config
--- other/admin/ipynb_deps.bash
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-find ./ -name '*.ipynb' -exec jupyter nbconvert --to script --stdout {} \; | grep -w import | sort | uniq
