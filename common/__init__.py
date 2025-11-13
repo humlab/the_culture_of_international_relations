27297b3043fe82a8f015a131e831ce7eb6a4d6ae
roger-mahler <roger.mahler@hotmail.com>
2025-11-13 16:36:50 +0100

    feat: add async setup_config function for configuration management

--- common/__init__.py
+++ common/__init__.py
@@ -1,8 +1,19 @@
 # type: ignore
 # Import order matters to avoid circular imports
 # Import widgets after utility to avoid circular imports
+import os
 
+import asyncio
+
+from . import resources
 from .color_utility import *  # noqa
+from .configuration import setup_config_store
 from .file_utility import FileUtility  # noqa
 from .utility import *  # noqa
 from .utility import plot_wordcloud  # noqa
+
+
+async def setup_config():
+    """Sets up the config module."""
+    filename: str = os.path.join(os.path.dirname(resources.__file__), 'config.yml')
+    await setup_config_store(filename=filename)
