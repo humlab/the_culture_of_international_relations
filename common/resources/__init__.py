112d8f9891612d0f098a91a92d48c16a79707cf4
roger-mahler <roger.mahler@hotmail.com>
2025-11-14 10:35:22 +0100

    fix: simplify file reading in get_fox_stopwords function

--- common/resources/__init__.py
+++ common/resources/__init__.py
@@ -7,14 +7,11 @@ def get_fox_stopwords() -> set[str]:
     """Reads file fox_stopwords.txt and returns a set of stopwords."""
     filepath: str = os.path.join(os.path.dirname(__file__), "fox_stopwords.txt")
     stopwords: set[str] = set()
-    try:
-        with open(filepath, encoding="utf-8") as f:
-            for line in f:
-                word: str = line.strip()
-                if word:
-                    stopwords.add(word)
-    except Exception:
-        raise
+    with open(filepath, encoding="utf-8") as f:
+        for line in f:
+            word: str = line.strip()
+            if word:
+                stopwords.add(word)
     return stopwords
 
 
@@ -35,7 +32,7 @@ _world_regions: dict[int, list[str]] = {}
 
 
 def get_world_regions() -> dict[int, list[str]]:
-    global _world_regions
+    global _world_regions  # pylint: disable=global-statement
     if _world_regions:
         return _world_regions
     filepath: str = os.path.join(os.path.dirname(__file__), "world_regions.yml")
