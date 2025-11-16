8037101588fa005249c6e714798f23142b8ed47f
roger-mahler <roger.mahler@hotmail.com>
2025-11-16 10:24:11 +0100

    fix: correct formatting inconsistencies in notebook instructions

--- notebooks/quantitative_analysis/notebook_instructions.py
+++ notebooks/quantitative_analysis/notebook_instructions.py
@@ -24,7 +24,7 @@
 # - You can run the entire notebook in a single step by clicking on the menu Cell -> Run All. Note that this can take some time to finish. You can see how cells are executed in sequence via the indicator in the margin (i.e. "In [\*]" changes to "In [n]" where n is an integer).
 # - The cells can be edited if they are double-clicked, in which case the cell border turns green. Use the ESC key to escape edit mode (or click on any other cell).
 #
-# To restart the kernel (i.e. the computational engine assigned to your session), click on the menu Kernel -> Restart. 
+# To restart the kernel (i.e. the computational engine assigned to your session), click on the menu Kernel -> Restart.
 #
 
 # %% code_folding=[0] language="html"
@@ -50,14 +50,14 @@
 # - Fields 'group1' and 'group2' are ignored (many missing values). Instead group are fetched via party code from encoding found in the "groups" table.
 
 # %% [markdown]
-# ### Chart: Treaty Quantities by Selected Parties 
+# ### Chart: Treaty Quantities by Selected Parties
 # This chart displays the number of treaties per time period for each party or group of parties. The time period can be year or one of a predefined set of time period divisions. The default time period division is 1919-1944, 1945-1955, 1956-1966, and 1967-1972, the alternative division has 1940-1944 as an additional period. The third division is a single period between 1945 and 1972 (inclusive)
 #
 # Several parties can be selected using CTRL + click on left mouse button. Shift-Up/Down can also be used to select consecutive parties. Some predefined party sets exist in the "Preset" dropdown, and, when selected, the corresponding parties is selected in the "Parties" widget. Use the "Top #n" slider to display parties with most treaties for each period. Note that a value greater than 0 will disable the "Parties" widget since these to selections are mutually exclusive. Set slider back to 0 to enable the "Parties" widget.
 #
 # The treaty count is based on the following rules:
 # - Treaties outside of selected division are discarded
-# - When "Is Cultural" is selected than the only treaties included are those having "is cultural" flag in WTI index set to "Yes" 
+# - When "Is Cultural" is selected than the only treaties included are those having "is cultural" flag in WTI index set to "Yes"
 # - When "Topic is 7CULT" is selected than all treaties having topic equal to 7CULT are included using original topic value according to WTI i.e. no recoding!
 # - Candidate for removal: When "No filter" is selected then no topic filter is applied
 #
@@ -73,7 +73,7 @@
 #
 # See previous chart for a description of "period" and "parties" options (the same rules apply in this chart).
 #
-# Topics **not** specified in the selected "Category" are as default **excluded** from the counts. If the "Add OTHER topics" toggle is set then a new "OTHER" category is added as a summed up count for topics outside the selected topic category. 
+# Topics **not** specified in the selected "Category" are as default **excluded** from the counts. If the "Add OTHER topics" toggle is set then a new "OTHER" category is added as a summed up count for topics outside the selected topic category.
 #
 # Note that, as default when the "Recode 7CORR" is set, all treaties marked as "is_cultural" = "Yes" in the WTI index are assigned a new topic code "7CORR". The remaining treaties ´having topic 7CULT are not recoded (will keep this code).
 #
@@ -89,4 +89,4 @@
 #         'category-item-n': [list-of-topic-codes-in-category-n]
 #     },
 # ```
-# The **category-group-name**s will be visible in the "Category" dropdown list, and it is hence important for these names to be unique. It can be an arbitrary name. The **category-item**s is the name that will be visible on the chart legend as label for associated list of topic-codes. The **list-of-topic-codes**, finally, must be a list of valid topic codes in the WTI-index (+ 7CORR). Note that these lists within the same category-group should be mutually exclusive i.e. the same code should not be added to more than one group (will give an unpredicted result). Also note that correct braces **must** be used, and strings **must** be surrounded by "'", and all topic codes must also be in uppercase. Follow the same syntax as in the existing specifications. 
+# The **category-group-name**s will be visible in the "Category" dropdown list, and it is hence important for these names to be unique. It can be an arbitrary name. The **category-item**s is the name that will be visible on the chart legend as label for associated list of topic-codes. The **list-of-topic-codes**, finally, must be a list of valid topic codes in the WTI-index (+ 7CORR). Note that these lists within the same category-group should be mutually exclusive i.e. the same code should not be added to more than one group (will give an unpredicted result). Also note that correct braces **must** be used, and strings **must** be surrounded by "'", and all topic codes must also be in uppercase. Follow the same syntax as in the existing specifications.
